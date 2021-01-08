15c9daf1d3c32e8b0cf70443d80015c911bacbff
Felipe Silva <felipe.dacs@gmail.com>
2021-01-07 23:49:22 -0300

    :tv: add aula 8

--- contas-correntes.php
+++ contas-correntes.php
@@ -4,31 +4,35 @@ function exibeMensagem($mensagem){
     echo $mensagem . PHP_EOL;
 }
 
+function sacar($conta, $valorASacar)
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não pode sacar este valor");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
 $contasCorrentes = [
     '123.456.789-10' => [
-        'titular' => 'Vinicius',
-        'saldo' => 1000
-    ],
-    '123.456.789-11' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ],
-    '123.256.789-12' => [
+    '123.456.789-11' => [
         'titular' => 'Alberto',
         'saldo' => 300
+    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
     ]
 ];
 
-if (500 > $contasCorrentes['123.456.789-10']['saldo']) {
-    exibeMensagem("Você não pode sacar este valor");
-} else
-    $contasCorrentes['123.456.789-10']['saldo'] -= 500;
 
-
-if (500 > $contasCorrentes['123.456.789-11']['saldo']) {
-    exibeMensagem("Você não pode sacar este valor");
-} else
-    $contasCorrentes['123.456.789-11']['saldo'] -= 500;
+$contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 500);
+$contasCorrentes['123.456.789-11'] = sacar($contasCorrentes['123.456.789-11'], 500);
 
 foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
